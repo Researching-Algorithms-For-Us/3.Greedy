f2a981fa0aa3e3f49ae64373e1d6734856239402
Mutesk <j0803@naver.com>
2021-04-23 19:58:42 +0900

    BJ 11047 Problem Solved For Testing

--- baekjoon/cpp/11047.cpp
+++ baekjoon/cpp/11047.cpp
@@ -1,1 +1,35 @@
-/* source code */
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int32_t> CoinTypes;
+    int UseCoinCount = 0;
+    int N, K;
+    
+    cin >> N >> K;
+    CoinTypes.resize(N);
+
+    for(int i=0; i<N; ++i)
+    {
+        int CoinType;
+        cin >> CoinTypes[i];
+    }
+
+    for(auto iter = CoinTypes.rbegin(); iter!= CoinTypes.rend(); ++iter)
+    {
+        const auto CoinType = *iter;
+
+        if(CoinType <= K)
+        {
+            UseCoinCount += K / CoinType;
+            K -= K / CoinType * CoinType;
+        }
+    }
+
+    cout << UseCoinCount << "\n";
+
+    return 0;
+}
